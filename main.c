56638856ac11f9b0beda372fad8e6e2582865065
Adrian Parvin Ouano <me@adrianpar.vin>
2025-05-06 16:35:33 +0800

    Implement capture function with suggestions

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #if defined(_WIN32) || defined(WIN32)
 #include <windows.h>
@@ -24,6 +26,56 @@ struct move {
     unsigned int length;
     unsigned int positions[MAX_MOVES];
 };
+
+void debug_move(struct move *move) {
+    printf("Move: ");
+    for (int i = 0; i < move->length; ++i) {
+        printf("%d ", move->positions[i]);
+    }
+    printf("\n");
+}
+
+struct moves {
+    struct move move;
+    struct moves *next;
+};
+
+struct move *add_move(struct moves **list) {
+    struct moves *moves = malloc(sizeof *moves);
+    moves->next = *list;
+    *list = moves;
+
+    return &moves->move;
+}
+
+void cleanup_moves(struct moves **moves) {
+    struct moves *move = *moves;
+
+    while (move != NULL) {
+        struct moves *next = move->next;
+        free(move);
+        move = next;
+    }
+
+    *moves = NULL;
+}
+
+int pdn_to_index(int pdn) {
+    --pdn;
+    int i = pdn / 4;
+    int j = 2 * (pdn % 4) + (i + 1) % 2;
+
+    return 8 * i + j;
+}
+
+int index_to_pdn(int index) {
+    int i = index / 8;
+    int j = index % 8;
+
+    j /= 2;
+
+    return 4 * i + j + 1;
+}
 
 // https://stackoverflow.com/a/47406222
 #ifdef OS_Windows
@@ -64,6 +116,16 @@ void init_game(struct game *b) {
     b->turn = WHITE;
 }
 
+bool is_valid_position(int position) {
+    if (position < 0 || position >= 64)
+        return false;
+
+    int i = position / 8;
+    int j = position % 8;
+
+    return (i % 2) != (j % 2);
+}
+
 void print_game(struct game *b) {
     for (int i = 7; i >= 0; --i) {
         for (int j = 7; j >= 0; --j) {
@@ -78,8 +140,8 @@ struct move *push_move(struct move *move, unsigned int position) {
     return move;
 }
 
-struct move *new_move(int start, int next, enum move_type move_type) {
-    struct move *move = malloc(sizeof *move);
+struct move *
+init_move(struct move *move, int start, int next, enum move_type move_type) {
     move->type = move_type;
     move->length = 0;
     push_move(move, start);
@@ -112,19 +174,12 @@ struct move *parse_move(char *move) {
         break;
     }
 
-    --start;
-    --next;
-
     // Convert a PDN position into an index to an 8x8 2D array
-    unsigned int start_i = start / 4;
-    unsigned int start_j = 2 * (start % 4) + (start_i + 1) % 2;
-    unsigned int start_index = 8 * start_i + start_j;
+    unsigned int start_index = pdn_to_index(start);
+    unsigned int next_index = pdn_to_index(next);
 
-    unsigned int next_i = next / 4;
-    unsigned int next_j = 2 * (next % 4) + (next_i + 1) % 2;
-    unsigned int next_index = 8 * next_i + next_j;
-
-    struct move *parsed_move = new_move(start_index, next_index, move_type);
+    struct move *parsed_move = malloc(sizeof *parse_move);
+    init_move(parsed_move, start_index, next_index, move_type);
 
     if (move_type_c == '-') {
         return parsed_move;
@@ -134,15 +189,11 @@ struct move *parse_move(char *move) {
         return parsed_move;
 
     while (sscanf(move + cursor, "x%u%n", &next, &bytes_read) == 1) {
-        --next;
-
-        cursor += bytes_read;
-
-        next_i = next / 4;
-        next_j = 2 * (next % 4) + (next_i + 1) % 2;
-        next_index = 8 * next_i + next_j;
+        next_index = pdn_to_index(next);
 
         push_move(parsed_move, next_index);
+
+        cursor += bytes_read;
     }
 
     char eos = move[cursor + bytes_read];
@@ -186,6 +237,10 @@ int execute_move(struct game *game, struct move *move) {
 
     for (int i = 1; i < move->length; ++i) {
         int next_pos = move->positions[i];
+
+        if (game->board[next_pos] != ' ') {
+            return -1;
+        }
 
         int direction;
         if ((next_pos - cur_pos) % 7 == 0) {
@@ -236,22 +291,112 @@ int execute_move_str(struct game *game, char *move) {
     return retval;
 }
 
+struct moves *find_captures(struct game *game) {
+    struct moves *list = NULL;
+
+    char *captures = game->turn == WHITE ? "bB" : "wW";
+
+    for (int i = 0; i < 8; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            char piece = game->board[8 * i + j];
+
+            if (piece == ' ' || strchr(captures, piece))
+                continue;
+
+            bool promoted = strchr("BW", piece) != NULL;
+
+            int directions[] = {7, 9, -7, -9};
+
+            for (int k = 0; k < 4; ++k) {
+                int direction = directions[k];
+
+                int start = 8 * i + j;
+                int cursor;
+                for (cursor = start + direction; is_valid_position(cursor);
+                     cursor += direction) {
+                    if (game->board[cursor] != ' ') {
+                        break;
+                    }
+                }
+                if (!is_valid_position(cursor)) {
+                    continue;
+                }
+
+                if (!strchr(captures, game->board[cursor])) {
+                    continue;
+                }
+
+                for (cursor += direction; is_valid_position(cursor);
+                     cursor += direction) {
+                    int steps = (cursor - start) / direction;
+
+                    if (!promoted && steps != 2) {
+                        break;
+                    }
+
+                    if (game->board[cursor] != ' ') {
+                        break;
+                    }
+
+                    struct move *move = add_move(&list);
+                    init_move(move, start, cursor, CAPTURE);
+                }
+            }
+        }
+    }
+
+    return list;
+}
+
 int main(void) {
     struct game game;
+    struct moves *captures;
 
     init_game(&game);
 
     execute_move_str(&game, "9-14");
-    execute_move_str(&game, "21-17");
+    // execute_move_str(&game, "21-17");
     print_game(&game);
+    captures = find_captures(&game);
 
     for (;;) {
         char move[100];
         fgets(move, 100, stdin);
 
-        int retval = execute_move_str(&game, move);
+        int retval;
+
+        if (captures) {
+            int choice;
+            sscanf(move, "%d", &choice);
+
+            struct moves *move = captures;
+            while (choice > 1) {
+                --choice;
+                move = move->next;
+            }
+
+            retval = execute_move(&game, &move->move);
+        } else {
+            retval = execute_move_str(&game, move);
+        }
+        cleanup_moves(&captures);
+        
         clear_screen();
         print_game(&game);
+        captures = find_captures(&game);
+
+        int i = 1;
+        for (struct moves *move = captures; move != NULL; move = move->next) {
+            printf(
+                "%d. %dx%d", i++, index_to_pdn(move->move.positions[0]),
+                index_to_pdn(move->move.positions[1])
+            );
+
+            for (int j = 2; j < move->move.length; ++j) {
+                printf("x%d", index_to_pdn(move->move.positions[j]));
+            }
+        }
+        printf("\n");
 
         if (retval == -1) {
             printf("Invalid move\n");
